64e400ec4b3f390d660948687bc7c4dc3f14c2e8
fsponton <francisco.sponton@gmail.com>
2023-03-30 16:22:09 +0200

    added client_request on root folder

--- module_bussines/src/controllers/find_user.js
+++ module_bussines/src/controllers/find_user.js
@@ -2,8 +2,8 @@ const User = require("../models/UserSchema")
 
 const find_user = async (req, res) => {
     try {
-        const { email } = req.query
-        const user = await User.findOne({ email: email }, { _id: 1, email: 1 });
+        const email = req.query
+        const user = await User.findOne({ email: email.email }, { _id: 1, email: 1 });
         return res.status(200).json(user)
     } catch (err) {
         return res.status(500).json({ status: "error", message: err.message })
--- module_users/src/controllers/find_user.js
+++ module_users/src/controllers/find_user.js
@@ -3,9 +3,10 @@ const validator = require('validator');
 
 const findByEmail = async (req, res) => {
     try {
-        const { email } = req.query
-        if (!validator.isEmail(email[0])) return res.status(400).json({ status: "error", message: "incorrect email" })
-        const user = await axios(`http://localhost:8888/api/negocio/user?email=${email[0]}`)
+        const email = req.query.email
+
+        if (!validator.isEmail(email)) return res.status(400).json({ status: "error", message: "incorrect email" })
+        const user = await axios(`http://localhost:8888/api/negocio/user?email=${email}`)
         return res.status(200).json(user.data)
     } catch (err) {
         return res.status(500).json({ status: "error", message: err.message })
